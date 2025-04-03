cc2331f02eb9f4747a05604da086ebac2459b5ae
TheWhiteLoafProto <73355924+PhobosMkII@users.noreply.github.com>
2025-04-03 11:28:17 -0400

    whar
    blah

---
+++ alua1.js
@@ -0,0 +1,13 @@
+let pinapple = 10
+let lime = 5
+let result1
+let result2
+let result3
+
+result1 = pinapple * lime
+result2 = lime / pinapple
+result3 = lime - pinapple
+
+const orange = "John"
+
+console.log(result1, result2, result3)
